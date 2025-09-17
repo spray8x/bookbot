478449c3182cd5d20b64510feded23f57c58f213
spray8x <vjpd2107@proton.me>
2025-09-17 05:18:41 +0000

    C: fixed shitty code; Add stats.py; moved get_num_words func to stats.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from stats import get_num_words
+
 def get_book_text(bookname):
     with open("books/"+bookname+".txt") as f:
-        file_contents = f.read()
+        
+        file_contents = f.read().replace('\n',' ').replace('    ', ' ').replace('   ', ' ').replace('  ', ' ')
+
     return file_contents
 
 def main():
-    print(get_book_text("frankenstein"))
+    print(get_num_words(get_book_text("frankenstein")), "words found in the document")
     
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(x):
+    l = x.split(" ")
+    return len(l)
